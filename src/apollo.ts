45d20bb20358be46e40d40bcd59eb2a8cb52e0c7
이기림(Kirim Lee)/FE개발팀/SKP <kirim@sk.com>
2018-12-18 09:18:12 +0900

    아폴로 클라이언트 클라이언트 스테이트

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,6 +1,38 @@
 import ApolloClient, { Operation } from 'apollo-boost';
 
 const client = new ApolloClient({
+    clientState: {
+        defaults: {
+            auth: {
+                __typename: 'Auth',
+                isLoggedIn: Boolean(localStorage.getItem('jwt'))
+            }
+        },
+        resolvers: {
+            Mutation: {
+                logUserIn: (_, { token }, {cache}) => {
+                    localStorage.setItem('jwt', token);
+                    cache.writeData({
+                        data: {
+                            __typename: "Auth",
+                            isLoggedIn: true
+                        }
+                    });
+                    return null;
+                },
+                logUserOut: (_, __, {cache}) => {
+                    localStorage.removeItem('jwt');
+                    cache.writeData({
+                        data: {
+                            __typename: "Auth",
+                            isLoggedIn: false
+                        }
+                    })
+                    return null;
+                }
+            }
+        }
+    },
     request: async(operation: Operation) => {
         operation.setContext({
             'X-JWT': localStorage.getItem('jwt') || ''
