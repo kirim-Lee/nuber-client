5a3616c9e85b3273b2994e897252f762cb626ce6
이기림(Kirim Lee)/FE개발팀/SKP <kirim@sk.com>
2018-12-27 10:50:54 +0900

    apollo를 이용해 codegen

--- README.md
+++ README.md
@@ -7,6 +7,12 @@ Client for the (N)Uber Clone Course on Nomad Academy. ReactJS, Apollo, Typescrip
 
 ## utils
 - react-toastify : 알림창 
+- apollo-cli :
+  - 설치 apollo 
+  - package.json 
+    - "precodegen":  apollo service:download --endpoint=http://localhost:4000/graphql
+    - 자동으로 schema.json 생성 (서버를 실행시킨상태에서 명령을 입력해주어야 한다. 기본적으로 타입을 알려줌)
+    - 
 
 ## tips
 - as any
--- src/Routes/PhoneLogin/PhoneLoginContainer.tsx
+++ src/Routes/PhoneLogin/PhoneLoginContainer.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Mutation } from 'react-apollo';
 import { RouteComponentProps } from 'react-router';
 import { toast } from 'react-toastify';
+import { PHONE_SIGN_IN } from './Phone.queries';
 import PhoneLoginPresenter from './PhoneLoginPresenter';
 
 interface IState{
     countryCode: string;
     phoneNumber: string;
 }
+
+interface IMutationInterface {
+    phoneNumber: string;
+}
+class PhoneSignInMutation extends Mutation<any, IMutationInterface> {}
 
 class PhoneLoginContainer extends React.Component<
     RouteComponentProps<any>, 
@@ -19,12 +26,33 @@ class PhoneLoginContainer extends React.Component<
     public render(){
         const {countryCode, phoneNumber} = this.state;
         return (
-            <PhoneLoginPresenter 
-                countryCode = { countryCode }
-                phoneNumber = { phoneNumber }
-                onInputChange = {this.onInputChange}
-                handleSubmit = {this.handleSubmit}
-            />
+            <PhoneSignInMutation 
+                mutation={PHONE_SIGN_IN} 
+                variables={{
+                    phoneNumber:`${countryCode}${phoneNumber}`
+                }}
+            >
+                {(mutation, {loading}) => {
+                    const handleSubmit: React.FormEventHandler<HTMLFormElement | HTMLButtonElement> = (event) => {        
+                        event.preventDefault();
+                        const isValid = /^\+[1-9]{1}[0-9]{7,11}$/.test(`${countryCode}${phoneNumber}`);
+                        if (isValid) {
+                            mutation();
+                        } else {
+                            toast.error('wrong phone number');
+                        }
+                        
+                        // toast.info("Suup");
+                    }
+                    return (
+                    <PhoneLoginPresenter 
+                        countryCode = { countryCode }
+                        phoneNumber = { phoneNumber }
+                        onInputChange = {this.onInputChange}
+                        handleSubmit = {handleSubmit}
+                    />)
+                }}
+            </PhoneSignInMutation>
         );
     }
 
@@ -38,18 +66,7 @@ class PhoneLoginContainer extends React.Component<
 
     }
 
-    public handleSubmit: React.FormEventHandler<HTMLFormElement | HTMLButtonElement> = (event) => {        
-        event.preventDefault();
-        const { countryCode, phoneNumber} = this.state;
-        const isValid = /^\+[1-9]{1}[0-9]{7,11}$/.test(`${countryCode}${phoneNumber}`);
-        if (isValid) {
-            return ;
-        } else {
-            toast.error('wrong phone number');
-        }
-        
-        // toast.info("Suup");
-    }
+    
 }
 
 export default PhoneLoginContainer;
